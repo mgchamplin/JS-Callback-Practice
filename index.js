ad086534da1bf80f37cf00d3b6beacdaf3da2bf0
Michael Champlin <mchamplin@sbcglobal.net>
2022-02-01 15:02:58 -0800

    First submission

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
-
-const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
-
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
@@ -13,3 +7,37 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+    console.log("NEW GIF:  " + character.src)
+}
+
+/* Start at (100,800) coordinates.  First call to withArrowKeys will move offset (0,0)
+*/
+move(character).to(100, 800)
+move(character).withArrowKeys(0, 0, handleDirectionChange)
+
+setInterval(move(character).moveIncrement, 1500)                                  
+
+
+
--- move.js
+++ move.js
@@ -1,3 +1,7 @@
+let direction = 'south';
+let x = 100;            /* Starting point on the screen */
+let y = 800;
+
 function move(element) {
     element.style.position = 'fixed'
 
@@ -6,7 +10,73 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveCharacter() {
+        if(direction === 'west'){ 
+            x = x - 100
+        }
+        if(direction === 'north'){
+            y = y + 100
+        }
+        if(direction === 'east'){
+            x = x + 100
+        }
+        if(direction === 'south'){
+            y = y - 100
+        }
+
+        if (y < 100) {
+            y = 100;
+        }
+        else if (y > 800) {
+            y = 800;
+        }
+        if (x < 100) {
+            x = 100;
+        }
+        else if (x > 900) {
+            x = 900;
+        }
+        console.log("Direction=" + direction + "   New X,Y  " + x + " " + y)
+
+        moveToCoordinates(x,y)
+    }
+    
+    function moveWithArrowKeys(left, bottom, callBackFx) {
+
+        callBackFx(direction)
+        console.log("MWAK: " + left + " " + bottom + " new dir=" + direction)
+
+        document.addEventListener('keydown', function(e){   
+            if (e.repeat) return;
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+
+            console.log("KEY: " +  direction)
+
+            callBackFx(direction)       /* Handle image change to new direction */
+
+            moveCharacter()
+        })
+
+        document.addEventListener('keyup', function(e){
+            console.log("Key Up")
+            //direction = null
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys,
+        moveIncrement:moveCharacter
     }
 }
